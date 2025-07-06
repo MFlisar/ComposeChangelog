73281826fd2a6016ac23a28afdb30e6ad57c3f1a
MFlisar <mflisar.development@gmail.com>
2025-07-06 16:15:29 +0200

    added mkdocs buildDocs task

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,3 +27,38 @@ tasks.register("publishToMaven") {
 tasks.register("publishToMavenLocal") {
     dependsOn(gradle.includedBuild("gradle-plugin").task(":plugin:publishToMavenLocal"))
 }
+
+// exclude all demo projects from CI builds
+subprojects {
+    if (project.path.contains(":demo:", ignoreCase = true) && System.getenv("CI") == "true") {
+        tasks.configureEach {
+            enabled = false
+        }
+    }
+}
+
+// ------------------------
+// Build mkdocs
+// ------------------------
+
+buildscript {
+    dependencies {
+        classpath(deps.kmp.gradle.tools.docs)
+    }
+}
+
+tasks.register("buildDocs") {
+    doLast {
+        // read env from build-mkdocs.yml
+        val generatedDocsDir = project.findProperty("generatedDocsDir") as String? ?: "gen/docs"
+        com.michaelflisar.kmpgradletools.docs.buildDocs(
+            relativePathDocsCustom = "documentation/custom",
+            relativePathGeneratedDocsOutput = generatedDocsDir,
+            relativeModulesPath = "library",
+            relativeDemosPath = "demo",
+            customOtherProjectsYamlUrl = "https://raw.githubusercontent.com/MFlisar/kmp-template/refs/heads/main/data/other-projects.yml"
+        )
+
+        println("Docs have been build!")
+    }
+}
