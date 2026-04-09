8ff9e835a363cd7450deffe5e8ea3b7f1ef8483e
MFlisar <mflisar.development@gmail.com>
2026-04-09 09:48:35 +0200

    ...

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,7 +41,7 @@ pluginManagement {
 
 plugins {
     // version catalogue does not work here!
-    id("io.github.mflisar.kmpdevtools.plugins-settings-gradle") version "7.8.2"
+    id("io.github.mflisar.kmpdevtools.plugins-settings-gradle") version "7.8.4"
 }
 
 // --------------
